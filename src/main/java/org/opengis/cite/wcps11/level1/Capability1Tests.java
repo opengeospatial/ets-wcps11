0091f92091bada2af0fed580bb16c5576816b047
Brook Balkachew Nigatu <brookbnigatu@gmail.com>
2026-04-15 17:06:54 +0200

    added coverage-coverage binary induced ops. These don't work with rasdaman

--- src/main/java/org/opengis/cite/wcps11/level1/Capability1Tests.java
+++ src/main/java/org/opengis/cite/wcps11/level1/Capability1Tests.java
@@ -84,7 +84,8 @@ public class Capability1Tests extends CommonFixture {
     @DataProvider(name = "binaryInducedOps")
     public Object[][] provideBinaryInducedOps() {
         return new Object[][]{{BOOLEAN_OP_ON_COV}, {SCALAR_ADDITION_ON_COV},
-                              {MUL}, {DIV}, {EQ}, {NEQ}, {GREATER}, {LESS}, {GREATER_EQ}, {LESS_EQ}};
+                              {MUL_SCALAR}, {DIV_SCALAR}, {EQ_SCALAR}, {NEQ_SCALAR}, {GREATER_SCALAR}, 
+                              {LESS_SCALAR}, {GREATER_EQ_SCALAR}, {LESS_EQ_SCALAR}};
     }
 
     @DataProvider(name = "switchExprScalar")
--- src/main/java/org/opengis/cite/wcps11/testdata/BinaryInduced.java
+++ src/main/java/org/opengis/cite/wcps11/testdata/BinaryInduced.java
@@ -15,6 +15,30 @@ public class BinaryInduced {
     
     private static final String INDUCED_BINARY_OP_WITH_SCALAR_ORACLE_TEMPLATE =
             "[{0},{0},{0},{0},{0}]";
+
+    private static final String INDUCED_BINARY_OP_WITH_COV_QUERY_TEMPLATE = 
+            "let $a := coverage newCov1\r\n" + //
+                        "domain crs \"OGC:Index3D\" with\r\n" + //
+                        "x index (0:1),\r\n" + //
+                        "y index (0:1),\r\n" + //
+                        "z index (0:1)\r\n" + //
+                        "range type\r\n" + //
+                        "elem quantity int\r\n" + //
+                        "range set\r\n" + //
+                        "{0},\r\n" + //
+                  "$b := coverage newCov2\r\n" + //
+                        "domain crs \"OGC:Index3D\" with\r\n" + //
+                        "x index (0:1),\r\n" + //
+                        "y index (0:1),\r\n" + //
+                        "z index (0:1)\r\n" + //
+                        "range type\r\n" + //
+                        "elem quantity int\r\n" + //
+                        "range set\r\n" + //
+                        "{1}\r\n" + //
+                        "return encode({2}, \"application/json\")";
+    
+    private static final String INDUCED_BINARY_OP_WITH_COV_ORACLE_TEMPLATE = 
+            "[[[{0},{0}],[{0},{0}]],[[{0},{0}],[{0},{0}]]]";
     
     private static QueryAndOracle injectArgumentsScalar(String constant, String expression, String expected) {
         return new QueryAndOracle(
@@ -23,19 +47,43 @@ public class BinaryInduced {
         );
     }
 
-    public static final QueryAndOracle MUL = injectArgumentsScalar("2", "3 * $a", "6");
+    private static QueryAndOracle injectArgumentsCov(String cov1Constant, String cov2Constant, String expression, String expected) {
+        return new QueryAndOracle(
+            MessageFormat.format(INDUCED_BINARY_OP_WITH_COV_QUERY_TEMPLATE, cov1Constant, cov2Constant, expression),
+            MessageFormat.format(INDUCED_BINARY_OP_WITH_COV_ORACLE_TEMPLATE, expected)
+        );
+    }
 
-    public static final QueryAndOracle DIV = injectArgumentsScalar("10", "$a / 2", "5");
+    public static final QueryAndOracle MUL_SCALAR = injectArgumentsScalar("2", "3 * $a", "6");
 
-    public static final QueryAndOracle EQ = injectArgumentsScalar("2", "2 = $a", "true");
+    public static final QueryAndOracle DIV_SCALAR = injectArgumentsScalar("10", "$a / 2", "5");
 
-    public static final QueryAndOracle NEQ = injectArgumentsScalar("2", "$a != 2", "false");
+    public static final QueryAndOracle EQ_SCALAR = injectArgumentsScalar("2", "2 = $a", "true");
 
-    public static final QueryAndOracle GREATER = injectArgumentsScalar("2", "3 > $a", "true");
+    public static final QueryAndOracle NEQ_SCALAR = injectArgumentsScalar("2", "$a != 2", "false");
 
-    public static final QueryAndOracle LESS = injectArgumentsScalar("4", "$a < 3", "false");
+    public static final QueryAndOracle GREATER_SCALAR = injectArgumentsScalar("2", "3 > $a", "true");
 
-    public static final QueryAndOracle GREATER_EQ = injectArgumentsScalar("2", "1 >= $a", "false");
+    public static final QueryAndOracle LESS_SCALAR = injectArgumentsScalar("4", "$a < 3", "false");
 
-    public static final QueryAndOracle LESS_EQ = injectArgumentsScalar("2", "$a <= 2", "true");
+    public static final QueryAndOracle GREATER_EQ_SCALAR = injectArgumentsScalar("2", "1 >= $a", "false");
+
+    public static final QueryAndOracle LESS_EQ_SCALAR = injectArgumentsScalar("2", "$a <= 2", "true");
+
+    public static final QueryAndOracle MUL_COV = injectArgumentsCov("2", "2", "$a * $b", "4");
+
+    public static final QueryAndOracle DIV_COV = injectArgumentsCov("9", "3", "$a / $b", "3");
+
+    public static final QueryAndOracle EQ_COV = injectArgumentsCov("2", "2", "$a = $b", "true");
+
+    public static final QueryAndOracle NEQ_COV = injectArgumentsCov("2", "3", "$a != $b", "true");
+
+    public static final QueryAndOracle GREATER_COV = injectArgumentsCov("3", "2", "$a > $b", "true");
+
+    public static final QueryAndOracle LESS_COV = injectArgumentsCov("5", "5", "$a < $b", "false");
+
+    public static final QueryAndOracle GREATER_EQ_COV = injectArgumentsCov("2", "2", "$a >= $b", "true");
+
+    public static final QueryAndOracle LESS_EQ_COV = injectArgumentsCov("2", "1", "$a <= $b", "false");
+
 }
